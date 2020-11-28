b48f3b5ee17c68aceef8ec073f4abc147f6a6836
fakhriPRD <pradhana.fakhri@gmail.com>
2020-11-28 22:41:16 +0700

    navbar berhasil dibuat, sudah ada 3 fragment

--- app/src/main/java/com/android/mytani/NavigationBar.java
+++ app/src/main/java/com/android/mytani/NavigationBar.java
@@ -1,14 +1,61 @@
 package com.android.mytani;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class NavigationBar extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    ChipNavigationBar bottomNav;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_bar);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+        /*if (savedInstanceState == null){
+            bottomNav.setItemSelected(R.id.goTo_fragment_home, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, homeFragment)
+                    .commit();
+        }*/
+        bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int id) {
+
+                // GET FRAGMENT ID TO MOVE FRAGMENT POSITION
+                Fragment fragment = null;
+                switch (id){
+                    case R.id.goTo_fragment_home:
+                    fragment = new HomeFragment();
+                    break;
+                    case R.id.goTo_fragment_discover:
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.goTo_fragment_profile:
+                        fragment = new ProfileFragment();
+                }
+
+                // REPLACE FRAME LAYOUT WITH SELECTED FRAGMENT
+                if (fragment != null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                } else{
+                    Log.e(TAG, "Error dalam membuat fragment");
+                }
+            }
+        });
     }
 }
